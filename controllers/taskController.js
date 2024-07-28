9239c496de3e3783d778f2b3dac06cfdf45e60d4
MinhAnh553 <huynhminhanh553@gmail.com>
2024-07-28 12:13:38 +0700

    Sắp xếp theo tiêu chí

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,15 +3,21 @@ const taskModel = require('../models/taskModel');
 // [GET] /api/v1/task
 module.exports.getAllTasks = async (req, res) => {
     try {
-        let find = {
+        const find = {
             deleted: false,
         };
+
+        const sort = {};
 
         if (req.query.status) {
             find.status = req.query.status;
         }
 
-        const tasks = await taskModel.find(find);
+        if (req.query.sortKey && req.query.sortValue) {
+            sort[req.query.sortKey] = req.query.sortValue;
+        }
+
+        const tasks = await taskModel.find(find).sort(sort);
         res.json(tasks);
     } catch (error) {
         res.json('Không tìm thấy!');
